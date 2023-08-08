971e5d19d09bfce7f36976eb4a32400a3ee29f73
Kimi Nel <kimi.monique.nel@gmail.com>
2023-08-08 22:45:44 +0200

    feat: added mobile skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -8,7 +8,11 @@ const Skills = () => {
       <h2 className="w-full font-bold lg:mt-40 mt-12 lg:pt-40 lg:mb-8 text-5xl lg:text-8xl text-center dark:text-light">
         Skills
       </h2>
-      <div className="w-full min-h-[400px] lg:h-[80vh] relative flex items-center justify-center rounded-full bg-circularLightSmall lg:bg-circularLightLarge dark:bg-circularDarkSmall lg:dark:bg-circularDarkLarge">
+
+      <div
+        id="desktop-view"
+        className="hidden w-full min-h-[400px] lg:h-[80vh] relative md:flex items-center justify-center rounded-full bg-circularLightSmall lg:bg-circularLightLarge dark:bg-circularDarkSmall lg:dark:bg-circularDarkLarge"
+      >
         <motion.div className="flex items-center p-8 justify-center rounded-full font-semibold bg-primary dark:bg-night text-light shadow-dark">
           Dev
         </motion.div>
@@ -109,6 +113,114 @@ const Skills = () => {
           name={'Figma'}
           xPosition={'-11vw'}
           yPosition={'3vw'}
+          href={'https://www.figma.com/'}
+        />
+      </div>
+
+      <div
+        id="mobile-view"
+        className="md:hidden w-full min-h-[400px] lg:h-[80vh] relative flex items-center justify-center rounded-full bg-circularLightSmall lg:bg-circularLightLarge dark:bg-circularDarkSmall lg:dark:bg-circularDarkLarge"
+      >
+        <motion.div className="flex items-center p-8 justify-center rounded-full font-semibold bg-primary dark:bg-night text-light shadow-dark">
+          Dev
+        </motion.div>
+
+        <Skill
+          name={'CSS'}
+          xPosition={'-7vw'}
+          yPosition={'-15vw'}
+          href={'https://developer.mozilla.org/en-US/docs/Web/CSS'}
+        />
+        <Skill
+          name={'HTML'}
+          xPosition={'5vw'}
+          yPosition={'-21vw'}
+          href={'https://developer.mozilla.org/en-US/docs/Web/HTML'}
+        />
+        <Skill
+          name={'TypeScript'}
+          xPosition={'27vw'}
+          yPosition={'-20vw'}
+          href={'https://www.typescriptlang.org/docs/'}
+        />
+        <Skill
+          name={'framer-motion'}
+          xPosition={'-30vw'}
+          yPosition={'0vw'}
+          href={'https://www.framer.com/motion/'}
+        />
+        <Skill
+          name={'HeadlessUI'}
+          xPosition={'-30vw'}
+          yPosition={'-20vw'}
+          href={'https://headlessui.com/'}
+        />
+        <Skill
+          name={'C#'}
+          xPosition={'37vw'}
+          yPosition={'13vw'}
+          href={'https://learn.microsoft.com/en-us/dotnet/csharp/'}
+        />
+        <Skill
+          name={'SQL'}
+          xPosition={'-10vw'}
+          yPosition={'32vw'}
+          href={'https://dev.mysql.com/'}
+        />
+        <Skill
+          name={'TypeORM'}
+          xPosition={'-23vw'}
+          yPosition={'23vw'}
+          href={'https://typeorm.io/'}
+        />
+        <Skill
+          name={'Docker'}
+          xPosition={'25vw'}
+          yPosition={'25vw'}
+          href={'https://docs.docker.com/'}
+        />
+        <Skill name={'React'} xPosition={'-2vw'} yPosition={'-40vw'} href={'https://react.dev/'} />
+        <Skill
+          name={'Tailwind CSS'}
+          xPosition={'-20vw'}
+          yPosition={'-30vw'}
+          href={'https://https://tailwindcss.com/'}
+        />
+        <Skill
+          name={'AWS'}
+          xPosition={'11vw'}
+          yPosition={'34vw'}
+          href={'https://docs.aws.amazon.com/'}
+        />
+        <Skill
+          name={'JavaScript'}
+          xPosition={'18vw'}
+          yPosition={'-30vw'}
+          href={'https://developer.mozilla.org/en-US/docs/Web/JavaScript'}
+        />
+        <Skill
+          name={'Node.js'}
+          xPosition={'22vw'}
+          yPosition={'8vw'}
+          href={'https://nodejs.org/en'}
+        />
+        <Skill
+          name={'Next.js'}
+          xPosition={'30vw'}
+          yPosition={'-7vw'}
+          href={'https://nextjs.org/'}
+        />
+        <Skill name={'GIT'} xPosition={'3vw'} yPosition={'24vw'} href={'https://git-scm.com/doc'} />
+        <Skill
+          name={'Canva'}
+          xPosition={'-21vw'}
+          yPosition={'-10vw'}
+          href={'https://www.canva.com/'}
+        />
+        <Skill
+          name={'Figma'}
+          xPosition={'-11vw'}
+          yPosition={'13vw'}
           href={'https://www.figma.com/'}
         />
       </div>
