17d586d33345206920746ead14b70203fa8d13f7
Kimi Nel <kimi.monique.nel@gmail.com>
2023-08-08 23:17:56 +0200

    style: Hero page padding

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ import NewTabArrow from '@/components/icons/NewTabArrow'
 const Hero = () => {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 space-y-6">
-      <div className="lg:pt-36 col-span-1 flex justify-center xl:justify-end">
+      <div className="lg:pt-32 col-span-1 flex justify-center xl:justify-end">
         <Image
           src={profilePic}
           alt="Kimi Nel"
@@ -17,7 +17,7 @@ const Hero = () => {
         />
       </div>
 
-      <div className="col-span-1 flex justify-center self-center">
+      <div className="col-span-1 flex justify-center self-center lg:pt-32">
         <div className="flex flex-col md:px-12 lg:px-0">
           <AnimatedText
             text={'Developing Your Visions'}
